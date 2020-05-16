d8c42bf8725f13ac576dde3f584a1c31d656eb03
pnogier <pnogiercontact@gmail.com>
2020-05-16 18:14:33 -0400

    :bug: #5 Adding cors to allow cross origin requests

--- server/index.js
+++ server/index.js
@@ -1,11 +1,13 @@
 const express = require('express'); // Load express to create the server
 const bodyParser = require('body-parser'); // Load body-parser to parse requests
 const fs = require('fs'); // Load node file system to serve our json files
+const cors = require('cors');
 
 const app = express(); // Create an express app
 
 app.use(bodyParser.json()); // Our app will only parse json
 app.use(bodyParser.urlencoded({ extended: true })); // Will use the qs library to parse the urlencoded
+app.use(cors()); // Make the express app use cors to allow cross domain get
 
 // Import routes from our routes file passing our express app and the actual file system as argument
 const routes = require('./routes/router')(app, fs);
