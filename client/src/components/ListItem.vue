80f18946ff6cc67abadbf214f4fc677fa9b4b3ff
pnogier <pnogiercontact@gmail.com>
2020-05-17 16:45:22 -0400

    :construction: :lipstick: #6 #7 Add the details modal, open and close the modal, icon button to open it from ListItem

--- client/src/components/ListItem.vue
+++ client/src/components/ListItem.vue
@@ -1,12 +1,12 @@
 <template>
-  <v-card class="mx-auto" max-width="500">
+  <v-card class="mx-auto mt-2" max-width="500">
     <v-row align="end" class="fill-height">
       <v-col align-self="center" class="px-7" cols="4">
         <v-avatar class="profile" color="grey" size="100">
           <v-img v-bind:src="user.avatar" />
         </v-avatar>
       </v-col>
-      <v-col class="py-0" align-self="center" cols="6">
+      <v-col class="py-0" align-self="center" cols="5">
         <v-list-item>
           <v-list-item-content>
             <v-list-item-title class="title">{{ user.firstName }} {{ user.lastName }}</v-list-item-title>
@@ -15,20 +15,53 @@
           </v-list-item-content>
         </v-list-item>
       </v-col>
-      <v-col cols="2" class="py-0" align-self="center">
-        <v-icon x-large align-self="center" v-if="user.gender === 'male'">fa-mars</v-icon>
-        <v-icon x-large align-self="center" v-if="user.gender === 'female'">fa-venus</v-icon>
+      <v-col cols="3" class="py-0" align-self="center">
+        <v-row justify="center">
+          <v-icon x-large color="#abcaff" align-self="center" v-if="user.gender === 'male'">fa-mars</v-icon>
+          <v-icon
+            x-large
+            color="#f2cdd7"
+            align-self="center"
+            v-if="user.gender === 'female'"
+          >fa-venus</v-icon>
+        </v-row>
+        <v-row justify="center">
+          <!-- dialog value becoms true when button is clicked, then Dialog is opened -->
+          <v-btn class="mt-2" small color="#90d891" dark fab @click="dialog = true">
+            <v-icon medium>mdi-plus</v-icon>
+          </v-btn>
+        </v-row>
+        <v-dialog v-model="dialog" max-width="350">
+          <v-card class="mx-auto">
+            <!-- Render DetailsPage passing current user as a prop -->
+            <DetailsPage :user="user" />
+            <v-card-actions>
+              <v-spacer />
+              <!-- dialog value become false when the dialog is closed -->
+              <v-btn color="gray darken-1" text @click="dialog = false">Close</v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-dialog>
       </v-col>
     </v-row>
   </v-card>
 </template>
 
 <script>
+import DetailsPage from "./pages/DetailsPage"; // Import DetailsPage component
 // Export ListItem component
 export default {
   name: "ListItem",
   props: {
     user: Object // The user prop received is an Object
+  },
+  components: {
+    DetailsPage // DetailsPage component is used in here
+  },
+  data() {
+    return {
+      dialog: false // Set a dialog value to handle the opening and closing of the dialog
+    };
   }
 };
 </script>
--- client/src/components/pages/DetailsPage.vue
+++ client/src/components/pages/DetailsPage.vue
@@ -1,1 +1,95 @@
-TODO
+<template>
+  <v-container>
+    <v-row justify="center">
+      <v-avatar class="profile mt-5" color="grey" size="100">
+        <v-img v-bind:src="user.avatar" />
+      </v-avatar>
+    </v-row>
+    <v-row class="mx-auto" justify="center">
+      <v-card-title class="headline">{{ user.firstName }} {{ user.lastName }}</v-card-title>
+    </v-row>
+    <v-row justify="center">
+      <v-simple-table height="300px">
+        <tbody>
+          <tr>
+            <td>
+              <strong>First name</strong>:
+            </td>
+            <td>{{ user.firstName }}</td>
+          </tr>
+          <tr>
+            <td>
+              <strong>Last name</strong>:
+            </td>
+            <td>{{ user.lastName }}</td>
+          </tr>
+          <tr>
+            <td>
+              <strong>Country</strong>:
+            </td>
+            <td>{{ user.country }}</td>
+          </tr>
+          <tr>
+            <td>
+              <strong>Age</strong>:
+            </td>
+            <td>{{ user.age }}</td>
+          </tr>
+          <tr>
+            <td>
+              <strong>Email</strong>:
+            </td>
+            <td>{{ user.email }}</td>
+          </tr>
+          <tr>
+            <td>
+              <strong>Gender</strong>:
+            </td>
+            <td>
+              <v-icon
+                medium
+                color="#abcaff"
+                align-self="center"
+                v-if="user.gender === 'male'"
+              >fa-mars</v-icon>
+              <v-icon
+                medium
+                color="#f2cdd7"
+                align-self="center"
+                v-if="user.gender === 'female'"
+              >fa-venus</v-icon>
+            </td>
+          </tr>
+        </tbody>
+      </v-simple-table>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+// Export DetailsPage component
+export default {
+  name: "DetailsPage",
+  props: {
+    user: Object // Receives user as an object
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
